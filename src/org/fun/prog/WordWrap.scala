a0ee1619c947d4e46d7c51eacf588a72c94a1ae3
Volodymyr Korolyov <vkorolyov@tycoint.com>
2017-07-27 10:06:20 +0100

    Fixed code for edge case

--- src/org/fun/prog/WordWrap.scala
+++ src/org/fun/prog/WordWrap.scala
@@ -2,47 +2,55 @@ package org.fun.prog
 
 //noinspection TypeAnnotation
 object WordWrap extends App {
+  type Line = List[String]
 
-  def wrap(text: String, width: Int): List[String] = {
-    wrap(text.split(' ').toList, width, Nil).reverse
+  def wrap(text: String, width: Int): String = {
+    val lines = wrap(text.split(' ').toList, width, List(List()))
+    lines
+      .reverse
+      .map(_.reverse.mkString(" "))
+      .mkString("\n")
   }
 
-  def wrap(words: List[String], width: Int, accLines: List[String]): List[String] = {
+  def wrap(words: List[String], width: Int, accLines: List[Line]): List[Line] = {
     (words, accLines) match {
       case (Nil, _) => accLines
-      case (word :: wordsTail, Nil) => wrap(wordsTail, width, word :: Nil)
       case (word :: wordsTail, line :: lineTail) => (word, line, width) match {
 
-        case LongWordSplits(wordEnd, newLine) => wrap(wordEnd :: wordsTail, width, newLine :: line :: lineTail)
+        case LongWordSplits(wordEnd, newLine) => wrap(wordEnd :: wordsTail, width, newLine :: accLines)
 
         case WordFitsLine(_, updatedLine) => wrap(wordsTail, width, updatedLine :: lineTail)
 
-        case _ => wrap(wordsTail, width, word :: line :: lineTail)
+        case _ => wrap(wordsTail, width, List(word) :: accLines)
       }
     }
   }
 
   val LongWordSplits = Extractor({
-    case (word, line, width) if word.length > width => Some((word.substring(width - 1), word.substring(0, width - 1) + "-"))
+    case (word, line, width) if word.length > width => Some((word.substring(width), List(word.substring(0, width))))
     case _ => None
   })
 
   val WordFitsLine = Extractor({
-    case (word, line, width)  if word.length + line.length <= width => Some("", line + " " + word)
+    case (word, line, width)  if lineLength(word :: line) <= width => Some("", word :: line)
     case _ => None
   })
 
-  case class Extractor(f: ((String, String, Int)) => Option[(String, String)]) {
-    def unapply(v: (String, String, Int)):Option[(String, String)] = f(v)
+  def lineLength( line: Line) = line.mkString(" ").length
+
+  case class Extractor(f: ((String, Line, Int)) => Option[(String, Line)]) {
+    def unapply(v: (String, Line, Int)):Option[(String, Line)] = f(v)
   }
 
   def format(text: String, width: Int): Unit = {
     println(text)
     println("---")
-    wrap(text, width).foreach(println)
+    println(wrap(text, width))
     println
   }
 
   format("aaa bb cc ddddd", 6)
   format("a verylongsingle word", 6)
+  format("word word", 3)
+  format("word word word", 9)
 }
